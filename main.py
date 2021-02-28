20b431f9c3cf3f51eb975b2257520968bbfeb888
abnuo <66645994+abnuo@users.noreply.github.com>
2021-02-28 02:02:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 os.environ["WAIT"] = "1"
 imgur = subprocess.Popen(["python","imgur.py"])
 doc = open("PENISES.txt","a")
-os.system('chmod 777 ncat; chmod 777 paping; chmod +s paping;')
+os.system('chmod 777 ncat; chmod 777 nping; chmod +s nping;')
 def genIP():
     ipGroup = []
     for i in range(4):
@@ -27,7 +27,7 @@ def pingIP(trip,port):
         time.sleep(int(os.environ["WAIT"]))
         ip = genIP()
         print("I Am Do" + str(ip))
-        ping = os.system("./paping " + str(ip))
+        ping = os.system("./nping " + str(ip))
         
         if ping == 1:
             print("EPIC FAIL! for " + ip )
